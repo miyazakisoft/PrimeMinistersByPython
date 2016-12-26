1a6bb11b06d57cb79c9abb8f92a17f5e4f07178b
miyazakisoft <special.research2016@gmail.com>
2016-12-27 00:08:30 +0900

    refs #6 IOを作成した

--- primeministers/io.py
+++ primeministers/io.py
@@ -8,8 +8,18 @@ class IO(object):
 
 	def read_csv(self, filename):
 		"""指定されたファイルをCSVとして読み込む。"""
-		return None
+
+		reader_list=[]
+
+    	reader = csv.reader(open(filename, 'r'))
+		for row in reader:
+			reader_list.append(row)
+		return reader_list
 
 	def write_csv(self, filename, rows):
 		"""指定されたファイルにCSVとして行たち(rows)を書き出す。"""
+
+		writer = csv.writer(open(filename, 'r'))
+		for i in rows:
+			writer.writerow(i)
 		return

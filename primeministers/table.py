1578eb9674534b931e4943684c7714c609f774cb
mituba <mituba@ccgstb157.kyoto-su.ac.jp>
2016-12-27 00:04:08 +0900

    refs #5 Tableを作成する

--- primeministers/table.py
+++ primeministers/table.py
@@ -8,28 +8,48 @@ class Table(object):
 
 	def __init__(self, kind_string):
 		"""テーブルのコンストラクタ。"""
+        self._kind_string = kind_string
+        self._attributes = attributes.Attributes(kind_string)
+        self._images = []
+        self._thumbnails = []
+        self._tuples = []
+
 		return
 
 	def __str__(self):
 		"""自分自身を文字列にして、それを応答する。"""
-		return None
+        a_string = self.__class__.__name__
+        a_string += "\n"
+        a_string += "Attributes:"
+        a_string += ",".join(self._attributes)
+        a_string += "images:"
+        a_string += ",".join(self._images)
+        a_string += "thumbnails:"
+        a_string += ",".join(self._thumbnails)
+        a_string += "images:"
+        a_string += ",".join(self._tuples)
+		return
 
 	def add(self, tuple):
 		"""タプルを追加する。"""
+        # values = tuple.values()
+        # keys = tuple.attributes().keys()
+        # if values[keys.index('image')] != '画像':
+        self._tuples.append(tuple)
 		return
 
 	def attributes(self):
 		"""属性リストを応答する。"""
-		return None
+		return self._attributes
 
 	def image_filenames(self):
 		"""画像ファイル群をリストにして応答する。"""
-		return None
+		return self._images
 
 	def thumbnail_filenames(self):
 		"""縮小画像ファイル群をリストにして応答する。"""
-		return None
-	
+		return self._thumbnails
+
 	def tuples(self):
 		"""タプル群を応答する。"""
-		return None
+		return self._tuples
